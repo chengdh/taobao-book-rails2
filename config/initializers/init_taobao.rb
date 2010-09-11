b040ef8bba9ce364ec2d1fa40ef8537b9de76660
chengdh <cheng.donghui@gmail.com>
2010-09-11 17:08:51 +0800

    comment init_taobao.rb

--- config/initializers/init_taobao.rb
+++ config/initializers/init_taobao.rb
@@ -1,4 +1,4 @@
 #从淘宝同步一些基础信息
-taobao_sess = Taobao::Session.new
-ItemCat.synchronize(taobao_sess)
-LocalArea.synchronize(taobao_sess)
+#taobao_sess = Taobao::Session.new
+#ItemCat.synchronize(taobao_sess)
+#LocalArea.synchronize(taobao_sess)
