cebd27f14c66d7732eeb1914a8975341c0101fb3
chengdh <cheng.donghui@gmail.com>
2010-09-11 16:42:55 +0800

    add .gems file

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 ActiveRecord::Schema.define(:version => 20100910005205) do
 
   create_table "douban_books", :force => true do |t|
-    t.string   "nick",         :limit => 60,  :default => "", :null => false
+    t.string   "nick",         :limit => 60,  :null => false
     t.string   "title",        :limit => 60
     t.string   "subtitle",     :limit => 60
     t.string   "aka",          :limit => 60
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
   end
 
   create_table "item_imgs", :force => true do |t|
-    t.integer  "img_id",     :limit => 8,                   :null => false
-    t.integer  "num_iid",    :limit => 8,                   :null => false
-    t.string   "url",        :limit => 100, :default => "", :null => false
+    t.integer  "img_id",     :limit => 8,   :null => false
+    t.integer  "num_iid",    :limit => 8,   :null => false
+    t.string   "url",        :limit => 100, :null => false
     t.integer  "position"
     t.datetime "created"
     t.datetime "created_at"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
 
   create_table "item_props", :primary_key => "pid", :force => true do |t|
     t.integer  "cid",            :limit => 8,                     :null => false
-    t.string   "name",           :limit => 60, :default => "",    :null => false
+    t.string   "name",           :limit => 60,                    :null => false
     t.integer  "parent_pid",     :limit => 8
     t.integer  "parent_vid",     :limit => 8
     t.boolean  "is_key_prop",                  :default => false
@@ -76,9 +76,9 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
   create_table "item_pvs", :force => true do |t|
     t.integer  "num_iid",    :limit => 8,                     :null => false
     t.integer  "pid",        :limit => 8,                     :null => false
-    t.string   "prop_name",  :limit => 60, :default => "",    :null => false
+    t.string   "prop_name",  :limit => 60,                    :null => false
     t.integer  "vid",        :limit => 8
-    t.string   "name",       :limit => 60, :default => "",    :null => false
+    t.string   "name",       :limit => 60,                    :null => false
     t.string   "name_alias", :limit => 60
     t.boolean  "is_parent",                :default => false
     t.string   "status",     :limit => 20
@@ -97,9 +97,9 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
   create_table "items", :primary_key => "num_iid", :force => true do |t|
     t.string   "iid",            :limit => 32
     t.string   "detail_url",     :limit => 200
-    t.string   "title",          :limit => 60,                                      :default => "",       :null => false
-    t.string   "nick",           :limit => 60,                                      :default => "",       :null => false
-    t.string   "item_type",      :limit => 20,                                      :default => "",       :null => false
+    t.string   "title",          :limit => 60,                                                            :null => false
+    t.string   "nick",           :limit => 60,                                                            :null => false
+    t.string   "item_type",      :limit => 20,                                                            :null => false
     t.integer  "cid",            :limit => 8,                                                             :null => false
     t.text     "desc",           :limit => 16777215,                                                      :null => false
     t.string   "pic_url",        :limit => 200
@@ -142,8 +142,8 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
   end
 
   create_table "local_areas", :force => true do |t|
-    t.integer  "area_type",                                :null => false
-    t.string   "name",       :limit => 60, :default => "", :null => false
+    t.integer  "area_type",                :null => false
+    t.string   "name",       :limit => 60, :null => false
     t.integer  "parent_id",  :limit => 8
     t.string   "zip",        :limit => 20
     t.datetime "created_at"
@@ -151,8 +151,8 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
   end
 
   create_table "postage_modes", :force => true do |t|
-    t.integer  "postage_id",   :limit => 8,                                                 :null => false
-    t.string   "postage_type", :limit => 20,                                :default => "", :null => false
+    t.integer  "postage_id",   :limit => 8,                                 :null => false
+    t.string   "postage_type", :limit => 20,                                :null => false
     t.decimal  "price",                      :precision => 15, :scale => 2
     t.decimal  "increase",                   :precision => 15, :scale => 2
     t.datetime "created_at"
@@ -160,15 +160,15 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
   end
 
   create_table "postages", :primary_key => "postage_id", :force => true do |t|
-    t.string   "name",             :limit => 60,                                 :default => "", :null => false
-    t.string   "memo",             :limit => 200,                                :default => "", :null => false
+    t.string   "name",             :limit => 60,                                 :null => false
+    t.string   "memo",             :limit => 200,                                :null => false
     t.decimal  "post_price",                      :precision => 15, :scale => 2
     t.decimal  "post_increase",                   :precision => 15, :scale => 2
     t.decimal  "express_price",                   :precision => 15, :scale => 2
     t.decimal  "express_increase",                :precision => 15, :scale => 2
     t.decimal  "ems_price",                       :precision => 15, :scale => 2
     t.decimal  "ems_increase",                    :precision => 15, :scale => 2
-    t.string   "nick",             :limit => 60,                                 :default => "", :null => false
+    t.string   "nick",             :limit => 60,                                 :null => false
     t.datetime "created"
     t.datetime "modified"
     t.datetime "created_at"
@@ -177,7 +177,7 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
 
   create_table "prop_values", :primary_key => "vid", :force => true do |t|
     t.integer  "pid",        :limit => 8
-    t.string   "name",       :limit => 60, :default => "", :null => false
+    t.string   "name",       :limit => 60,                :null => false
     t.string   "name_alias", :limit => 60
     t.boolean  "is_parent"
     t.string   "status",     :limit => 20
@@ -207,8 +207,8 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
   end
 
   create_table "shops", :primary_key => "sid", :force => true do |t|
-    t.integer  "cid",          :limit => 8,                   :null => false
-    t.string   "nick",         :limit => 20,  :default => "", :null => false
+    t.integer  "cid",          :limit => 8,   :null => false
+    t.string   "nick",         :limit => 20,  :null => false
     t.string   "title",        :limit => 200
     t.string   "desc",         :limit => 500
     t.string   "bulletin",     :limit => 500
@@ -223,9 +223,9 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
   create_table "sku_pvs", :force => true do |t|
     t.integer  "sku_id",     :limit => 8,                     :null => false
     t.integer  "pid",        :limit => 8,                     :null => false
-    t.string   "prop_name",  :limit => 60, :default => "",    :null => false
+    t.string   "prop_name",  :limit => 60,                    :null => false
     t.integer  "vid",        :limit => 8,                     :null => false
-    t.string   "name",       :limit => 60, :default => "",    :null => false
+    t.string   "name",       :limit => 60,                    :null => false
     t.string   "name_alias", :limit => 60
     t.boolean  "is_parent",                :default => false
     t.string   "status",     :limit => 20
@@ -246,8 +246,7 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
     t.datetime "updated_at"
   end
 
-  create_table "syn_logs", :id => false, :force => true do |t|
-    t.string   "nick",          :default => "", :null => false
+  create_table "syn_logs", :primary_key => "nick", :force => true do |t|
     t.datetime "last_syn_time"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -274,8 +273,8 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
   end
 
   create_table "users", :primary_key => "user_id", :force => true do |t|
-    t.string   "uid",                 :limit => 30, :default => "",    :null => false
-    t.string   "nick",                :limit => 60, :default => "",    :null => false
+    t.string   "uid",                 :limit => 30,                    :null => false
+    t.string   "nick",                :limit => 60,                    :null => false
     t.string   "sex",                 :limit => 1
     t.integer  "buyer_credit_id"
     t.integer  "seller_credit_id"
@@ -283,7 +282,7 @@ ActiveRecord::Schema.define(:version => 20100910005205) do
     t.datetime "created"
     t.datetime "last_visit"
     t.date     "birthday"
-    t.string   "user_type",           :limit => 1,  :default => "",    :null => false
+    t.string   "user_type",           :limit => 1,                     :null => false
     t.boolean  "has_more_pic",                      :default => false
     t.integer  "item_img_num",                      :default => 0
     t.integer  "item_img_size",                     :default => 0
