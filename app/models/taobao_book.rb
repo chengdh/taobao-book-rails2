3d8a7db14b16a4edccccfde75464169a4da1d7d8
chengdh <cheng.donghui@gmail.com>
2010-09-12 15:43:19 +0800

    uncomment taobao_book input_pids

--- app/models/taobao_book.rb
+++ app/models/taobao_book.rb
@@ -50,12 +50,10 @@ class TaobaoBook < BaseItem
   def input_pids
     self.item_pvs.collect {|pv| pv.pid}.join(',')
     #TODO 暂时注释
-    ""
   end
   def input_str
     self.item_pvs.collect {|pv| pv.name}.join(',')
     #TODO 暂时注释
-    ""
   end
 
   #获取或设置商品属性
