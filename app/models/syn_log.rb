f3b2379d17fe46189c238f8e60a0c6921c7dc88b
chengdh <cheng.donghui@gmail.com>
2010-09-13 09:46:46 +0800

    change syn_log app subscribe code

--- app/models/syn_log.rb
+++ app/models/syn_log.rb
@@ -9,6 +9,16 @@ class SynLog < ActiveRecord::Base
       ret.first.valid == 'true'
     end
   end
+  #获取isv订阅信息
+  def self.get_subscribe_message(sess)
+    ret = sess.invoke("taobao.increment.subscribemessage.get","fields" => Taobao::SubscribeMessage.fields)
+    if ret.is_a? Taobao::ErrorRsp
+      nil
+    else
+      ret.first
+    end
+
+  end
   #用户授权ISV获取其增量信息
   def self.user_authorize(sess)
     ret = sess.invoke("taobao.increment.user.authorize","session" => sess.session_key,"duration" => 2 )
@@ -17,5 +27,10 @@ class SynLog < ActiveRecord::Base
     else
       ret.first.is_a? Taobao::AuthorizeMessage 
     end
+  end
+  #货物用户授权信息
+  def self.get_authorize_message(sess)
+    ret = sess.invoke("aobao.increment.authorizemessages.get","fields" => Taobao::AuthorizeMessage.fields,"nicks" => sess.top_params['visitor_nick'] )
+    ret
   end
 end
