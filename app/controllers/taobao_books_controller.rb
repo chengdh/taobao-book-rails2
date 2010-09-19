68e52f4f30507971b840eda71ddedd13a6f6d333
chengdh <cheng.donghui@gmail.com>
2010-09-19 11:08:56 +0800

    fixed synchronize bug

--- app/controllers/taobao_books_controller.rb
+++ app/controllers/taobao_books_controller.rb
@@ -15,7 +15,11 @@ class TaobaoBooksController < BaseController
     end
   end
   def search_douban
-    @debug_rsp = taobao_session.invoke("taobao.increment.items.get",'nick' => taobao_session.top_params["visitor_nick"],"start_modified" => "2010-09-19 00:00:00",'session' => taobao_session.session_key)
+
+    nick = sess.top_params['visitor_nick']
+    last_syn_time =  SynLog.find(nick).last_syn_time
+    start_modified = last_syn_time.strftime('%Y-%m-%d %H:%M:%S')
+    @debug_rsp = taobao_session.invoke("taobao.increment.items.get",'nick' => taobao_session.top_params["visitor_nick"],"start_modified" => start_modified,'session' => taobao_session.session_key)
 
   end
   #根据传入的ISBN编号,从豆瓣查书
