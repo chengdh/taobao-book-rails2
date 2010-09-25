6d8579e68fafb58fea0fd3ae62baa8ed4a86916f
chengdh <cheng.donghui@gmail.com>
2010-09-25 16:17:29 +0800

    fixed province select bug

--- app/controllers/provinces_controller.rb
+++ app/controllers/provinces_controller.rb
@@ -2,7 +2,7 @@ class ProvincesController < BaseController
   #根据传入的省份id获取城市列表
   #GET  local_areas/:id/children
   def children
-    @provinces = Province.find(params[:id])
+    @province = Province.find(params[:id])
     respond_to do |format|
      format.js {render :partial => "city"}
     end
