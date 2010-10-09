fb61b2b8cbd4f4bd4d2ec9e558fcad29e1eebb90
chengdh <cheng.donghui@gmail.com>
2010-10-09 10:35:55 +0800

    fixed syn bug

--- vendor/plugins/taobao/lib/taobao/service.rb
+++ vendor/plugins/taobao/lib/taobao/service.rb
@@ -2,6 +2,7 @@ require 'digest/md5'
 require 'net/http'
 require 'open-uri'
 require 'rest_client'
+require 'iconv'
 
 module Taobao
   class Service
@@ -23,9 +24,10 @@ module Taobao
       tmp_params.delete("image")
       @params.merge!(options)
       #将nick进行urlencode编码
-      tmp_params['nick'] = tmp_params['nick'].URLEncode if !tmp_params['nick'].blank?
-      tmp_params['nicks'] = tmp_params['nicks'].URLEncode if !tmp_params['nicks'].blank?
-      #@params['nick'] = @params['nick'].URLEncode if !@params['nick'].blank?
+      conv = Iconv.new('UTF-8','GBK')
+      tmp_params['nick'] = conv.iconv(tmp_params['nick']) if !tmp_params['nick'].blank?
+      #tmp_params['nicks'] = tmp_params['nicks'].URLEncode if !tmp_params['nicks'].blank?
+      @params['nick'] = conv.iconv(@params['nick']) if !@params['nick'].blank?
 
       str = ENV['TAOBAO_APP_SECRET'] + (tmp_params.sort.collect { |c| "#{c[0]}#{c[1]}" }).join("") + ENV['TAOBAO_APP_SECRET']
       @params["sign"] = Digest::MD5.hexdigest(str).upcase!
