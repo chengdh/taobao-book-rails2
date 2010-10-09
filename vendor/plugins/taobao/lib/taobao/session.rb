a20aa2aaa2d967a0d2e6084e0537d8600196e907
chengdh <cheng.donghui@gmail.com>
2010-10-09 11:09:42 +0800

    fixed syn bug

--- vendor/plugins/taobao/lib/taobao/session.rb
+++ vendor/plugins/taobao/lib/taobao/session.rb
@@ -16,8 +16,7 @@ module Taobao
         if sign == params['top_sign']
           self.session_key = params['top_session']
           conv = Iconv.new('UTF-8','GBK')
-          @top_params = Hash[*(Base64.decode64(params['top_parameters']).split('&').collect {|v| v.split('=')}).flatten]
-          @top_params['visitor_nick'] =  @top_params['visitor_nick'].URLDecode 
+          @top_params = Hash[*(Base64.decode64(params['top_parameters']).URLDecode.split('&').collect {|v| v.split('=')}).flatten]
           @authorized = true
         else
           throw InvalidSignature.new('top_sign签名验证非法!')
