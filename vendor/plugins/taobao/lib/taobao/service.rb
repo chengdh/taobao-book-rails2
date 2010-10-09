f4fe4ecd26e5b90bfbd9a914791ea8b26d9b132b
chengdh <cheng.donghui@gmail.com>
2010-10-09 09:37:07 +0800

    fixed syn bug

--- vendor/plugins/taobao/lib/taobao/service.rb
+++ vendor/plugins/taobao/lib/taobao/service.rb
@@ -25,6 +25,7 @@ module Taobao
       #将nick进行urlencode编码
       tmp_params['nick'] = tmp_params['nick'].URLEncode if !tmp_params['nick'].blank?
       tmp_params['nicks'] = tmp_params['nicks'].URLEncode if !tmp_params['nicks'].blank?
+      @params['nick'] = @params['nick'].URLEncode if !@params['nick'].blank?
 
       str = ENV['TAOBAO_APP_SECRET'] + (tmp_params.sort.collect { |c| "#{c[0]}#{c[1]}" }).join("") + ENV['TAOBAO_APP_SECRET']
       @params["sign"] = Digest::MD5.hexdigest(str).upcase!
